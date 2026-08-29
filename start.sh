d47738203a0727b3b65b44b6b5be548bf33bd713
PalashMendhe <palashmendhe777@gmail.com>
2026-08-29 10:48:56 +0530

    Repo Cleanup

--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source /opt/ros/lyrical/setup.bash
-source /home/plsh/robotic_arm/install/setup.bash
-ros2 launch arm_moveit_config moveit.launch.py use_rviz:=false > /home/plsh/robotic_arm/moveit_new.log 2>&1
